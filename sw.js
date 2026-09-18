5a120c360ef94ea3c460c5784a61a9faae63096a
siranildo518-coder <siranildo518@gmail.com>
2026-09-18 20:28:14 -0300

    Adiciona sw.js ao THOR MATRIZ

---
+++ sw.js
@@ -0,0 +1,2 @@
+// THOR 7 V117 TESTE - atualizacao em segundo plano sem recarregar a tela atual; login, senha e trava preservados sem alteracao.
+importScripts('./sw-core-thor7.js?v=thor7-v117-sem-topo-antigo-2');
